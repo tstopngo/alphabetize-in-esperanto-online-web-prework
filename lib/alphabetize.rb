469cb5514a16ec7e90c2e03137818b1f6bc909de
Tracy Ngo <tngo1206@gmail.com>
2019-03-11 00:19:08 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  arr.sort_by { |string| string.length}
+  arr.sort_by { |string| string[0] == ESPERANTO_ALPHABET.each do }
     
 end
