dfab3f3e8bde1ffe7f83fce14aefd797c5b1212c
Tracy Ngo <tngo1206@gmail.com>
2019-03-11 00:19:51 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  arr.sort_by { |string| string[0] == ESPERANTO_ALPHABET.each do }
+  arr.sort_by { |string| ESPERANTO_ALPHABET.to_a.include }
     
 end
