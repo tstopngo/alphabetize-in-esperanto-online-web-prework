4c8195090e70645931b523f1d1aa2fe9ec134d7e
Tracy Ngo <tngo1206@gmail.com>
2019-03-11 00:32:58 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+
 def alphabetize(arr)
   # code here
+  
+ arr.sort_by do |sentence|
+    sentence.split("").map do |character|
+      ESPERANTO_ALPHABET.index(character)
+    end
+  end
 end
